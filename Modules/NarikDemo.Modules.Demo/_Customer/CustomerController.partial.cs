2403d1e137f46f1e8aa6483ea1b374b6d90b79b7
ali badakhshan <abadakhshan@gmail.com>
2019-07-17 10:22:44 +0430

    add [EnableQuery] to CustomerController.GetForSelector

--- Modules/NarikDemo.Modules.Demo/_Customer/CustomerController.partial.cs
+++ Modules/NarikDemo.Modules.Demo/_Customer/CustomerController.partial.cs
@@ -1,10 +1,13 @@
 ﻿using System.Linq;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Authorization;
 using NarikDemo.Data.Model;
 
 namespace NarikDemo.Modules.Demo._Customer
 {
     public partial class CustomerController
     {
+        [EnableQuery]
         public IQueryable<CustomerDto> GetForSelector()
         {
             return DomainService.GetEntityList<Customer, CustomerDto>(x=>x.Individuals.Any());
